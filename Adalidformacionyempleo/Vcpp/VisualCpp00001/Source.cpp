7605525661025dc0d3c93c72736a579f895e9de8
jorge <geordi_j_laforge@hotmail.com>
2020-11-15 15:07:56 +0100

    Este codigo es base fucional para crear u minimo codigo con formaularioCodigo base de Vcpp con formulario  totalmente funcional,

---
+++ Adalidformacionyempleo/Vcpp/VisualCpp00001/Source.cpp
@@ -0,0 +1,12 @@
+#include "Myform.h" // Archivo de foemulario
+using namespace VisualCpp00001; // Nombre de l proyecto
+ 
+// Progrma priniapl 
+int main()
+{
+	MyForm mf; // 
+	mf.ShowDialog();
+
+	return 0; // devuelve si todo esta bien
+
+}
